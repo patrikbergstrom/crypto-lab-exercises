a1cc64c3f0ac5bbee180fa449522c0426f5c6efa
Patrik Bergström <patrik.bergstrom@gmail.com>
2013-10-06 17:12:06 +0200

    More difficult.

--- src/main/java/crypto/DSA.java
+++ src/main/java/crypto/DSA.java
@@ -10,33 +10,24 @@ public class DSA {
     private PrivateKey privateKey;
 
     public DSA() throws NoSuchAlgorithmException {
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("DSA");
-        kpg.initialize(1024);
-        KeyPair keyPair = kpg.generateKeyPair();
-        publicKey = keyPair.getPublic();
-        privateKey = keyPair.getPrivate();
+        // Generate keypair and initialize publicKey and privateKey.
     }
 
     public String buildSignature(String input) throws Exception {
 
-        Signature dsa = Signature.getInstance("DSA");
-
-        dsa.initSign(privateKey);
-
-        dsa.update(input.getBytes());
-
-        byte[] sign = dsa.sign();
+        /*
+        Gör allt som behövs för att signera
+         */
+        byte[] sign = new byte[0]; // remove
 
         return new String(Base64.encodeBase64(sign));
     }
 
     public boolean verify(String input, String signature) throws Exception {
 
-        Signature dsa = Signature.getInstance("DSA");
-        dsa.initVerify(publicKey);
-
-        dsa.update(input.getBytes());
-
-        return dsa.verify(Base64.decodeBase64(signature.getBytes()));
+        /*
+        Gör allt som behövs för att verifiera.
+         */
+        return false;
     }
 }
